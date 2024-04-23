20ec591a4a8bfa3b254fa4c8488a434ff9eea820
Tomasz <tom.p.smolen@gmail.com>
2024-04-23 21:20:57 +0200

    made it 10 times better and far less retarded

--- dijkstraaaa.py
+++ dijkstraaaa.py
@@ -2,35 +2,12 @@ def dijkstra(G, start, end):
     start.color = "gray"
     queue = [start]
     start.distance = 0
-    end_adj = []
-    for element in G.adj:
-        """
-        el stupido loopo resposible for checking collecting
-        all nodes that lead to the end node
-        so we can check if they all have been marked as black later
-        there has to be a better way to do this
-        """
-        adjecencies = G.adj[element]
-        for edge in adjecencies:
-            if end in edge:
-                end_adj.append(element)
     while queue != []:
         u = queue[0]
+        if u == end:
+            break
         if u.color != "black":
             adjecencies = G.adj[u]
-            all_black = True
-            for node in end_adj:
-                """
-                checks if all paths to the end have been checked
-                -> they are all black,
-                if so, return
-                """
-                if node.color != "black":
-                    all_black = False
-                    break
-            if all_black:
-                return
-
             for element in adjecencies:
                 node = element[0]
                 weight = element[1]
